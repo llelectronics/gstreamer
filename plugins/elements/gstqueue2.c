8222fb1e76deb003436ac1f59c92791266547967
Leszek Lesner <leszek.lesner@web.de>
2018-02-24 13:35:53 +0100

    Upped buffer size

--- plugins/elements/gstqueue2.c
+++ plugins/elements/gstqueue2.c
@@ -101,7 +101,7 @@ enum
 };
 
 /* other defines */
-#define DEFAULT_BUFFER_SIZE 4096
+#define DEFAULT_BUFFER_SIZE 8192
 #define QUEUE_IS_USING_TEMP_FILE(queue) ((queue)->temp_template != NULL)
 #define QUEUE_IS_USING_RING_BUFFER(queue) ((queue)->ring_buffer_max_size != 0)  /* for consistency with the above macro */
 #define QUEUE_IS_USING_QUEUE(queue) (!QUEUE_IS_USING_TEMP_FILE(queue) && !QUEUE_IS_USING_RING_BUFFER (queue))
@@ -109,9 +109,9 @@ enum
 #define QUEUE_MAX_BYTES(queue) MIN((queue)->max_level.bytes, (queue)->ring_buffer_max_size)
 
 /* default property values */
-#define DEFAULT_MAX_SIZE_BUFFERS   100  /* 100 buffers */
-#define DEFAULT_MAX_SIZE_BYTES     (2 * 1024 * 1024)    /* 2 MB */
-#define DEFAULT_MAX_SIZE_TIME      2 * GST_SECOND       /* 2 seconds */
+#define DEFAULT_MAX_SIZE_BUFFERS   200  /* 100 buffers */
+#define DEFAULT_MAX_SIZE_BYTES     (4 * 1024 * 1024)    /* 4 MB */
+#define DEFAULT_MAX_SIZE_TIME      4 * GST_SECOND       /* 4 seconds */
 #define DEFAULT_USE_BUFFERING      FALSE
 #define DEFAULT_USE_TAGS_BITRATE   FALSE
 #define DEFAULT_USE_RATE_ESTIMATE  TRUE
